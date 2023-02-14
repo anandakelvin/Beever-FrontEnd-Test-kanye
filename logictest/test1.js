02e95e4a6a068f0c6cf3acb59d718488e8298453
Kelvin Ananda <ananda.kelvin@gmail.com>
2023-02-14 16:07:15 +0700

    test1.js answered with a mark of not understood

--- logictest/test1.js
+++ logictest/test1.js
@@ -1,9 +1,11 @@
 let words = "beever";
 
-// Buatlah skema logika untuk memuat kata diatas menjadi berbentuk seperti berikut : 
+// Buatlah skema logika untuk memuat kata diatas menjadi berbentuk seperti berikut :
 // b
 // be
 // bee
 // beev
 // beeve
 // beever
+
+// Soal tidak dimengerti.
